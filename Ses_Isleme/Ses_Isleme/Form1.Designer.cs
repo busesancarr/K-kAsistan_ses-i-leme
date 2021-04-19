2e78234051f9941e29d78353b80bd6c408b7bef5
Buse Sancar <79580128+busesancarr@users.noreply.github.com>
2021-04-19 10:24:41 +0300

    Add files via upload

---
+++ Ses_Isleme/Ses_Isleme/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Ses_Isleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(24, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Please, You Speak";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.TextChanged += new System.EventHandler(this.button1_TextChanged);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(24, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Speak Asistant";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 204);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(233, 130);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label1.Location = new System.Drawing.Point(286, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(290, 98);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(598, 476);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Location = new System.Drawing.Point(0, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(100, 23);
+            this.linkLabel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 585);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Küçük Asistan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Ses_Isleme/Ses_Isleme/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using SpeechLib;
+
+namespace Ses_Isleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpVoice sp = new SpVoice();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+            Grammar g = new DictationGrammar();
+            sr.LoadGrammar(g);
+
+            try
+            {
+                button1.Text = "Konuşun";
+                sr.SetInputToDefaultAudioDevice();
+                RecognitionResult res = sr.Recognize();
+                button1.Text = res.Text;
+            }
+            catch (Exception)
+            {
+                button1.Text = "Anlaşılamadı";
+            }
+        }
+
+        private void button1_TextChanged(object sender, EventArgs e)
+        {
+            if (button1.Text == "Hello")
+            {
+                
+                label1.Text = "Hello how can I help you?";
+                sp.Speak("Hello how can I help you?");
+            }
+
+            if (button1.Text == "What is your name")
+            {
+
+                label1.Text = "My name is  Assistant";
+                sp.Speak("My name is Busenin Assistant");
+            }
+
+            if (button1.Text == "How are you")
+            {
+
+                label1.Text = "I am fine thank you. How are you?";
+                sp.Speak("I'm fine thank you. How are you?");
+            }
+            if (button1.Text == "Thank you")
+            {
+
+                label1.Text = "I thank you. It was nice to talk to you.";
+                sp.Speak("I thank you. It was nice to talk to you.");
+            }
+           
+
+            if (button1.Text == "Black")
+            {
+
+                label1.Text = "Black";
+                sp.Speak("Here the black color is displayed");
+                BackColor = Color.Black;
+            }
+
+            if (button1.Text == "Yellow")
+            {
+
+                label1.Text = "Yellow";
+                sp.Speak("Here the yellow color is displayed");
+                BackColor = Color.Yellow;
+            }
+
+            if (button1.Text == "Red")
+            {
+
+                label1.Text = "Red";
+                sp.Speak("Here the red color is displayed");
+                BackColor = Color.Red;
+            }
+            if (button1.Text == "Blue")
+            {
+
+                label1.Text = "Blue";
+                sp.Speak("Here the blue color is displayed");
+                BackColor = Color.Blue;
+            }
+
+       
+            if (button1.Text == "Germany")
+            {
+                sp.Speak("You can view everything about Germany here");
+                label1.Text = "You can view everything about Germany here";
+                webBrowser1.Navigate("https://www.google.com/search?q=Germany&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk03kPz1FyloOBT4-PCZDfqxjVr5Zpg%3A1618710503909&ei=5497YL_yNtW73AOYv6voAQ&oq=Germany&gs_lcp=Cgdnd3Mtd2l6EANQ5Y0LWKCWC2C2pQtoAHACeACAAf8liAH_JZIBAzktMZgBAKABAaoBB2d3cy13aXrAAQE&sclient=gws-wiz&ved=0ahUKEwj_gdHE1obwAhXVHXcKHZjfCh0Q4dUDCA4&uact=5");
+                
+            }
+
+            if (button1.Text == "Turkey")
+            {
+                sp.Speak("You can view everything about Turkey here");
+                label1.Text = "You can view everything about Turkey here";
+                webBrowser1.Navigate("https://www.google.com/search?q=T%C3%BCrkiye&oq=T%C3%BCrkiye&aqs=chrome..69i57j0i271l3j69i60j69i61l2.3641j0j7&sourceid=chrome&ie=UTF-8");
+
+            }
+            if (button1.Text == "Tokyo")
+            {
+                sp.Speak("You can view everything about Tokyo here");
+                label1.Text = "You can view everything about Tokyo here";
+                webBrowser1.Navigate("https://www.google.com/search?q=Tokyo&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk01f4brLiXe3XOOGj32d0qgq4bX7cg%3A1618698953850&ei=yWJ7YNajM9WHwPAPqoiC4AQ&oq=Tokyo&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjELADECcyBwgjELADECcyBwgjELADECcyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsANQ3uO4BVjK-LgFYLj5uAVoAXACeACAAZMDiAGTA5IBAzQtMZgBAKABAaoBB2d3cy13aXrIAQrAAQE&sclient=gws-wiz&ved=0ahUKEwiWrJHBq4bwAhXVAxAIHSqEAEwQ4dUDCA4&uact=5");
+                
+            }
+            if (button1.Text == "University")
+            {
+                webBrowser1.Navigate("https://www.google.com/search?q=aksaray+%C3%BCniversitesi&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk01xsYCrwvIx_gqtfsoZHv9_Ov6dJQ%3A1618711925014&ei=dZV7YK4Wt-Dv9Q-7oLjYBA&gs_ssp=eJzj4tLP1TdIt6xKjzcwYPQSTcwuTixKrFQ4vCcvsyy1qDizJLU4EwDRfAzw&oq=Aksaray+&gs_lcp=Cgdnd3Mtd2l6EAEYBjIGCCMQJxATMgQIIxAnMgQIIxAnMgUIABDLATIFCC4QywEyBQguEMsBMgUILhDLATIFCAAQywEyBQgAEMsBMgUIABDLAToFCAAQkQI6CwguEMcBEKMCEJECOgIIADoICC4QxwEQowI6AgguOgcIABCHAhAUULKcZFiFp2Rg1sFkaABwAngAgAHvA4gBgBOSAQkwLjMuMy4yLjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz");
+                sp.Speak("You can view everything about University Aksaray here");
+            }
+
+            if (button1.Text == "Software")
+            {
+                webBrowser1.Navigate(" https://www.google.com/search?q=Yaz%C4%B1l%C4%B1m+nedir&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk03MFigUxRm8aMkNsDmqTOUNvBpkSw%3A1618711280900&ei=8JJ7YLihNuePrgSdlZ7ABQ&oq=Yaz%C4%B1l%C4%B1m+nedir&gs_lcp=Cgdnd3Mtd2l6EAMyCggAEMsBEEYQ-QEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywE6BwgAEEcQsANQvDZYxkNgqkRoAXACeACAAcwCiAHrCZIBBzAuNC4xLjGYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=gws-wiz&ved=0ahUKEwi42ZC32YbwAhXnh4sKHZ2KB1gQ4dUDCA4&uact=5");
+                sp.Speak("You can view everything about Software here");
+            }
+
+            if (button1.Text == "Google")
+            {   
+                label1.Text = "Google opens";
+                System.Diagnostics.Process.Start("chrome.exe");
+            }
+
+            if (button1.Text == "Capital")
+            {
+                label1.Text = "Turkey's capital, Ankara";
+                linkLabel1.Text = "Ankara View";
+            }
+
+            if(button1.Text == "Paint")
+            {
+                System.Diagnostics.Process.Start("mspaint.exe");
+            }
+        }
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               if(richTextBox1.Text == "Google" || richTextBox1.Text == "google")
+                {
+                    sp.Speak("Opens Chrome");
+                    System.Diagnostics.Process.Start("chrome.exe");
+                }
+
+                if (richTextBox1.Text == "Paint" || richTextBox1.Text == "paint")
+                {
+                    sp.Speak("Opens Paint");
+                    System.Diagnostics.Process.Start("mspaint.exe");
+                }
+                if (richTextBox1.Text == "University" || richTextBox1.Text == "university")
+                {
+                    sp.Speak("You can view everything about University Aksaray here ");
+                    webBrowser1.Navigate("https://www.google.com/search?q=aksaray+%C3%BCniversitesi&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk01xsYCrwvIx_gqtfsoZHv9_Ov6dJQ%3A1618711925014&ei=dZV7YK4Wt-Dv9Q-7oLjYBA&gs_ssp=eJzj4tLP1TdIt6xKjzcwYPQSTcwuTixKrFQ4vCcvsyy1qDizJLU4EwDRfAzw&oq=Aksaray+&gs_lcp=Cgdnd3Mtd2l6EAEYBjIGCCMQJxATMgQIIxAnMgQIIxAnMgUIABDLATIFCC4QywEyBQguEMsBMgUILhDLATIFCAAQywEyBQgAEMsBMgUIABDLAToFCAAQkQI6CwguEMcBEKMCEJECOgIIADoICC4QxwEQowI6AgguOgcIABCHAhAUULKcZFiFp2Rg1sFkaABwAngAgAHvA4gBgBOSAQkwLjMuMy4yLjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz");
+                }
+                if (richTextBox1.Text == "Türkiye" || richTextBox1.Text == "türkiye" || richTextBox1.Text == "Turkey" || richTextBox1.Text == "turkey")
+                {
+                    sp.Speak("You can view everything about Turkey here");
+                    label1.Text = "You can view everything about Turkey here";
+                    webBrowser1.Navigate("https://www.google.com/search?q=T%C3%BCrkiye&oq=T%C3%BCrkiye&aqs=chrome..69i57j0i271l3j69i60j69i61l2.3641j0j7&sourceid=chrome&ie=UTF-8");
+                    
+                }
+                if (richTextBox1.Text == "Software" || richTextBox1.Text == "Yazılım" || richTextBox1.Text == "software" || richTextBox1.Text == "yazılım")
+                {
+                    sp.Speak("You can view everything about Software here");
+                    label1.Text = "You can view everything about Turkey here";
+                    webBrowser1.Navigate(" https://www.google.com/search?q=Yaz%C4%B1l%C4%B1m+nedir&safe=strict&bih=657&biw=1366&hl=en&sxsrf=ALeKk03MFigUxRm8aMkNsDmqTOUNvBpkSw%3A1618711280900&ei=8JJ7YLihNuePrgSdlZ7ABQ&oq=Yaz%C4%B1l%C4%B1m+nedir&gs_lcp=Cgdnd3Mtd2l6EAMyCggAEMsBEEYQ-QEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywE6BwgAEEcQsANQvDZYxkNgqkRoAXACeACAAcwCiAHrCZIBBzAuNC4xLjGYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=gws-wiz&ved=0ahUKEwi42ZC32YbwAhXnh4sKHZ2KB1gQ4dUDCA4&uact=5");
+                   
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            sp.Speak(richTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
